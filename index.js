804f630b3eaa3c2f65e91d121de8e63ed2242f0b
Levente Elek <126334853+LeventeElek92@users.noreply.github.com>
2023-03-19 19:11:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let V = "Kovács";
+
+let A = "Anna";
+let P = "Panna";
+let H = "Hanna";
+
+let KA = "Dolly";
+let KP = "Molly";
+let KH = "Fred";
+
+let C1 = "Samu";
+let C2 = "Alfréd";
+
+function wordCount(str){
+    return str.split( ' '). length;
+}
+
+console.log(wordCount("Dolly Molly Fred Samu Alfréd"));
+
+var string = " kutyája: "
+
+console.log(V + " " + A + ", " + V + " " + P + ", " + "és "  + V + " " + H);
+
+console.log(A + string + KA);
+console.log(P + string + KP);
+console.log(H + string + KH);
+
+var petNumber = 5;
+
+console.log("A Kovács lányok állatainak száma: " + petNumber);
---
+++ index2.js
@@ -0,0 +1,16 @@
+let a = 3;
+let b = 4;
+let c = 5;
+
+let D1 = 25;
+let D2 = 45;
+
+var perimeter = "A háromszög kerülete: ";
+var area = "A háromszög területe: ";
+var ThirdAngle = "A háromszög 3. belső szögének foka: "
+
+console.log(area + (a*b/2) + "cm2");
+
+console.log(perimeter + (a + b + c) + "cm");
+
+console.log(ThirdAngle + (180-D1-D2));
